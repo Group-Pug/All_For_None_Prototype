9e52860c998f7b546d2447bf1f2d3c235290b2ab
Michael Vermeulen <mike.vermeulen9@gmail.com>
2015-02-01 16:59:48 +0100

    Added some scene placeholders

---
+++ Assets/CharacterHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterHandler : MonoBehaviour {
+	private GameObject model;
+	[SerializeField] private float rotationSpeed = 50f;
+
+	// Update is called once per frame
+	void Update () {
+		if(model != null){
+			RotateCharacter ();
+		}else{
+			model = GameObject.FindGameObjectWithTag ("Character");
+		}
+	}
+	void RotateCharacter() {
+		model.transform.localEulerAngles += new Vector3(0, Time.deltaTime * rotationSpeed, 0);
+	}
+}
