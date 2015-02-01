8b167dc1605c73b1e6185179539bbdee045d06d5
Michael Vermeulen <mike.vermeulen9@gmail.com>
2015-02-01 14:03:00 +0100

    Initial Commit

---
+++ Assets/Scripts/Scene Switching/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	IEnumerator NextLevel() {
+		float fadeTime = GameObject.Find ("_GM").GetComponent<SceneFading>().BeginFade (1);
+		yield return new WaitForSeconds(fadeTime);
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+}
