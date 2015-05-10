3598c4a694aed2f5984ee2372b96154b1beff342
Michael Vermeulen <mike.vermeulen9@gmail.com>
2015-05-10 16:56:34 +1200

    Migrated to Unity 5

---
+++ Assets/Scripts/Main Menu/MenuHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuHandler : MonoBehaviour {
+	
+	public void StartGame() {
+		Application.LoadLevel(4);
+	}
+
+	public void ExitGame() {
+		//Add confirmation popup
+		Application.Quit();
+	}
+}
